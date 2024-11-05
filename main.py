5fc8b2b012d2c4183f0b62fc48e89fb31d41b43e
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-11-05 13:45:59 +0100

    Implement func to load instances

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def load_instance(input_file_name: str) -> None:
-    pass
+from curses.ascii import isalnum
+
+
+def load_instance(input_file_name: str) -> list[list[int]]:
+    global K, N, M
+    cells = []
+
+    with open(input_file_name, 'r') as file:
+        K = int(next(file))
+        N, M = map(int, next(file).split())
+        for line in file:
+            line = line.split()
+            if line:
+                line = [int(x) if isalnum(x) else 0 for x in line]
+                cells.append(line)
+
+    return cells
 
 def main():
     pass

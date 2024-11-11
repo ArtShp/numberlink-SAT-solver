1e1d47b247b4d85700c63ae35c73abb667b12fc7
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-11-11 22:30:51 +0100

    Add next condition
    Each cell has exactly one value

--- main.py
+++ main.py
@@ -47,6 +47,19 @@ def encode(instance: list[list[int]]) -> tuple[list[list[int]], int]:
         for j in range(M):
             clauses.append([-encode_var(0, i + 1, j + 1)])
 
+    # 3. Each cell has exactly one value
+    # 3.1. Each cell has at least one value
+    for i in range(N):
+        for j in range(M):
+            clauses.append([encode_var(k, i + 1, j + 1) for k in range(-K, K + 1)])
+
+    # 3.2. Each cell has at least one value
+    for i in range(N):
+        for j in range(M):
+            for k1 in range(-K, K + 1):
+                for k2 in range(k1 + 1, K + 1):
+                    clauses.append([-encode_var(k1, i + 1, j + 1), -encode_var(k2, i + 1, j + 1)])
+
     return clauses, number_of_variables
 
 def main():

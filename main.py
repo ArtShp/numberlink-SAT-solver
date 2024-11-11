223960994d9b78159d4b6fa868a87faf1336790c
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-11-11 22:46:34 +0100

    Fix typo

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def encode(instance: list[list[int]]) -> tuple[list[list[int]], int]:
         for j in range(M):
             clauses.append([encode_var(k, i + 1, j + 1) for k in range(-K, K + 1)])
 
-    # 3.2. Each cell has at least one value
+    # 3.2. Each cell has at most one value
     for i in range(N):
         for j in range(M):
             for k1 in range(-K, K + 1):

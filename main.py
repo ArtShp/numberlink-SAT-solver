ea590673ff049d5c3d1d9246ab9e41efba3ed080
ArtShp <artemiy.shipovalov2005@gmail.com>
2024-11-12 14:47:19 +0100

    Swap conditions to work properly
    More strict conditions first

--- main.py
+++ main.py
@@ -78,15 +78,7 @@ def encode(instance: list[list[int]]) -> tuple[list[list[int]], int]:
     for i in range(N):
         for j in range(M):
             if (k := instance[i][j]) != 0:
-                if 1 < i + 1 < N and 1 < j + 1 < M: # center
-                    # !1 or !2, !1 or !3, !1 or !4, !2 or !3, !2 or !4, !3 or !4
-                    clauses.append([-encode_var(k, *get_top_neighbour(i + 1, j + 1)), -encode_var(k, *get_left_neighbour(i + 1, j + 1))])
-                    clauses.append([-encode_var(k, *get_top_neighbour(i + 1, j + 1)), -encode_var(k, *get_bottom_neighbour(i + 1, j + 1))])
-                    clauses.append([-encode_var(k, *get_top_neighbour(i + 1, j + 1)), -encode_var(k, *get_right_neighbour(i + 1, j + 1))])
-                    clauses.append([-encode_var(k, *get_left_neighbour(i + 1, j + 1)), -encode_var(k, *get_bottom_neighbour(i + 1, j + 1))])
-                    clauses.append([-encode_var(k, *get_left_neighbour(i + 1, j + 1)), -encode_var(k, *get_right_neighbour(i + 1, j + 1))])
-                    clauses.append([-encode_var(k, *get_bottom_neighbour(i + 1, j + 1)), -encode_var(k, *get_right_neighbour(i + 1, j + 1))])
-                elif i + 1 == 1 and j + 1 == 1: # left top corner
+                if i + 1 == 1 and j + 1 == 1: # left top corner
                     # !3 or !4
                     clauses.append([-encode_var(k, *get_bottom_neighbour(i + 1, j + 1)), -encode_var(k, *get_right_neighbour(i + 1, j + 1))])
                 elif i + 1 == 1 and j + 1 == M: # right top corner
@@ -98,6 +90,15 @@ def encode(instance: list[list[int]]) -> tuple[list[list[int]], int]:
                 elif i + 1 == N and j + 1 == M: # right bottom corner
                     # !1 or !2
                     clauses.append([-encode_var(k, *get_top_neighbour(i + 1, j + 1)), -encode_var(k, *get_left_neighbour(i + 1, j + 1))])
+                elif 1 < i + 1 < N and 1 < j + 1 < M: # center
+                    # !1 or !2, !1 or !3, !1 or !4, !2 or !3, !2 or !4, !3 or !4
+                    clauses.append([-encode_var(k, *get_top_neighbour(i + 1, j + 1)), -encode_var(k, *get_left_neighbour(i + 1, j + 1))])
+                    clauses.append([-encode_var(k, *get_top_neighbour(i + 1, j + 1)), -encode_var(k, *get_bottom_neighbour(i + 1, j + 1))])
+                    clauses.append([-encode_var(k, *get_top_neighbour(i + 1, j + 1)), -encode_var(k, *get_right_neighbour(i + 1, j + 1))])
+                    clauses.append([-encode_var(k, *get_left_neighbour(i + 1, j + 1)), -encode_var(k, *get_bottom_neighbour(i + 1, j + 1))])
+                    clauses.append([-encode_var(k, *get_left_neighbour(i + 1, j + 1)), -encode_var(k, *get_right_neighbour(i + 1, j + 1))])
+                    clauses.append([-encode_var(k, *get_bottom_neighbour(i + 1, j + 1)), -encode_var(k, *get_right_neighbour(i + 1, j + 1))])
+
 
 
     return clauses, number_of_variables
